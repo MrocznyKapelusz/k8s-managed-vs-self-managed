5b365428b60e785a6b72a2111c0ab27f86b7b886
MrocznyKapelusz <kacper.starosciak@gmail.com>
2026-07-08 22:03:44 +0200

    Remove sensitive marker from vars.

--- infrastructure/eks/variables.tf
+++ infrastructure/eks/variables.tf
@@ -1,35 +1,29 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  sensitive   = true
 }
 
 variable "aws_deploy_role_arn" {
   description = "IAM role assumed by the AWS provider"
   type        = string
-  sensitive   = true
 }
 
 variable "aws_state_role_arn" {
   description = "IAM role assumed by the AWS provider"
   type        = string
-  sensitive   = true
 }
 
 variable "state_bucket_name" {
   description = "S3 bucket used for OpenTofu state"
   type        = string
-  sensitive   = true
 }
 
 variable "state_file_key" {
   description = "S3 object key used for OpenTofu state"
   type        = string
-  sensitive   = true
 }
 
 variable "aws_web_identity_token_file" {
   description = "Path to the GitHub OIDC token file"
   type        = string
-  sensitive   = true
 }
